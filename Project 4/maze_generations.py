6c590203e600c244746aa4d7597159af338eae45
Prince-Khatri-1275 <princekhatri1275@gmail.com>
2026-02-19 13:15:20 +0530

    Using sync neighbors for a little upgrade.

--- Project 4/maze_generations.py
+++ Project 4/maze_generations.py
@@ -1,6 +1,14 @@
 import numpy as np
 from constants import *
 
+# Adding a decorator to keep neighbors in sync after generation
+def sync_neighbors(func):
+    def wrapper(*args, **kwargs):
+        tiles = func(*args, **kwargs)
+        return keep_in_sync(tiles)
+    return wrapper
+
+@sync_neighbors
 def random_generation(tiles):
     for tile in tiles.values():
         k_neigh = np.random.randint(0, 5)
@@ -17,5 +25,29 @@ def random_generation(tiles):
         
     return tiles
 
+@sync_neighbors
 def dfs_for_maze_generation(tiles):
+    return tiles
+
+def keep_in_sync(tiles):
+    """
+    This is a helper function to keep the neighbors of the tiles in sync with each other.
+    For example if tile A has tile B as its neighbor then tile B should also have tile
+    """
+    for pos, tile in tiles.items():
+        for neighbor in tile.neighbors:
+            i, j = pos
+            match neighbor:
+                case "up":
+                    if (i-1, j) in tiles:
+                        tiles[(i-1, j)].update_neighbors("down")
+                case "down":
+                    if (i+1, j) in tiles:
+                        tiles[(i+1, j)].update_neighbors("up")
+                case "left":
+                    if (i, j-1) in tiles:
+                        tiles[(i, j-1)].update_neighbors("right")
+                case "right":
+                    if (i, j+1) in tiles:
+                        tiles[(i, j+1)].update_neighbors("left")
     return tiles
